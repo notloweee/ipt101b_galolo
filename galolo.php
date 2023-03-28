f96d962f7a3cc97c9157b39cb6071df76ac2ced6
notloweee <123623461+notloweee@users.noreply.github.com>
2023-03-28 16:22:04 +0800

    Add files via upload

---
+++ galolo.php
@@ -0,0 +1,22 @@
+<hmtl>
+<head><title>MULTIPLICATION</title>
+</head>
+<body>
+    <center>
+    
+    <br>
+    <h2>MULTIPLICATION TABLE OF THE NUMBER 5:</h2>
+    <br>
+    <?php
+        $number = 5;
+        $j = 10;
+
+    for($i=1; $i<=$j; $i++){
+            echo $number ." x ". $i ." = ". $number * $i ."<br><br>";
+        // echo "Yeah";
+        }
+    ?>
+    <h4>Created by: Louie Gerson S. Galolo</h4>
+    </center>
+</body>
+</html>
